661cceae069e87612e80cd029ae30a7e9744d7db
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-05-31 18:45:59 +0530

    CREATED CONTACTUS PAGE

--- clarivo/contact-us.php
+++ clarivo/contact-us.php
@@ -2,6 +2,188 @@
 
 
 
+<!-------------------------------Map------------------------------------------>
+<section>
+    <div class="contact-map mt-5">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.0934043340653!2d72.89505411382925!3d19.10355788707209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c8760d6311c9%3A0x8bb9589f83ffcc35!2sKothari%20Compound%2C%20Jangaleshwar%20Mahadev%20Mandir%20Rd%2C%20Subhash%20Nagar%2C%20Mohili%2C%20Asalpha%2C%20Mumbai%2C%20Maharashtra%20400072!5e0!3m2!1sen!2sin!4v1678098653196!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</section>
+
+<div class="vc_row-full-width vc_clearfix"></div>
+<div class="vc_row wpb_row vc_row-fluid vc_custom_1515386841533">
+    <div class="wpb_column vc_column_container vc_col-sm-12">
+        <div class="vc_column-inner">
+            <div class="wpb_wrapper">
+                <div class="vc_row wpb_row vc_inner vc_row-fluid">
+                    <div class="wpb_column vc_column_container vc_col-sm-12">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper">
+                                <h2 style="font-size: 30px;color: #18a0b1;text-align: center text-align: left" class="vc_custom_heading heading_style_1">Contact Detail
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="vc_row wpb_row vc_inner vc_row-fluid vc_custom_1515386847766" style="margin-top: 20px;">
+                    <div class="wpb_column vc_column_container vc_col-sm-4">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper">
+                                <div class=" ">
+                                    <div class="icon-box-shortcode-wrap style_1 ">
+                                        <div class="icon-box-container">
+                                            <div class="icon-wrap" style="color:#75b239">
+                                                <span class="fa fa-phone" style="color:#18a0b1;"></span>
+                                            </div>
+                                            <div class="icon-content">
+                                                <h5 class="icon-title"> Call Us </h5>
+                                                <p class="icon-description">+91 9867592446</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="wpb_column vc_column_container vc_col-sm-4">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper">
+                                <div class=" ">
+                                    <div class="icon-box-shortcode-wrap style_1 ">
+                                        <div class="icon-box-container">
+                                            <div class="icon-wrap" style="color:#75b239">
+                                                <span class="fa fa-map-marker" style="color:#18a0b1;"></span>
+                                            </div>
+                                            <div class="icon-content">
+                                                <h5 class="icon-title"> Adress </h5>
+                                                <p class="icon-description">Office No- 2 Plot No- 98, Sector 35 Dolphin Platinum Kamothe, Navi Mumbai, Maharashtra, India</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="wpb_column vc_column_container vc_col-sm-4">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper">
+                                <div class=" ">
+                                    <div class="icon-box-shortcode-wrap style_1 ">
+                                        <div class="icon-box-container">
+                                            <div class="icon-wrap" style="color:#75b239">
+                                                <span class="fa fa-envelope" style="color:#18a0b1;"></span>
+                                            </div>
+                                            <div class="icon-content">
+                                                <h5 class="icon-title"> Email </h5>
+                                                <p class="icon-description">info@mygenichealthcare.com</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="vc_row wpb_row vc_row-fluid vc_custom_1515386988109">
+    <div class="wpb_column vc_column_container vc_col-sm-12">
+        <div class="vc_column-inner">
+            <div class="wpb_wrapper">
+                <div class="vc_row wpb_row vc_inner vc_row-fluid">
+                    <div class="wpb_column vc_column_container vc_col-sm-12">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper">
+                                <h2 style="font-size: 30px;color: #18a0b1;text-align: center text-align: left; margin-bottom: 30px;" class="vc_custom_heading heading_style_1">Leave us a message</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="vc_row wpb_row vc_inner vc_row-fluid vc_custom_1515386994085">
+                    <div class="wpb_column vc_column_container vc_col-sm-2">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper"></div>
+                        </div>
+                    </div>
+                    <div class="wpb_column vc_column_container vc_col-sm-8">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper">
+                                <div role="form" class="wpcf7" id="wpcf7-f635-p588-o1" lang="en-US" dir="ltr">
+                                    <div class="screen-reader-response">
+                                        <p role="status" aria-live="polite" aria-atomic="true"></p>
+                                        <ul></ul>
+                                    </div>
+                                    <form action="https://demo.harutheme.com/clarivo/contact-us/#wpcf7-f635-p588-o1"
+                                        method="post" class="wpcf7-form init" novalidate="novalidate"
+                                        data-status="init">
+                                        <div style="display: none;">
+                                            <input type="hidden" name="_wpcf7" value="635" />
+                                            <input type="hidden" name="_wpcf7_version" value="5.5.6" />
+                                            <input type="hidden" name="_wpcf7_locale" value="en_US" />
+                                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f635-p588-o1" />
+                                            <input type="hidden" name="_wpcf7_container_post" value="588" />
+                                            <input type="hidden" name="_wpcf7_posted_data_hash" value="" />
+                                        </div>
+                                        <div class="contact-form">
+                                            <div class="contact-name">
+                                                <div><span class="wpcf7-form-control-wrap your-name"><input type="text"
+                                                            name="your-name" value="" size="40"
+                                                            class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                                            aria-required="true" aria-invalid="false"
+                                                            placeholder="Your name *" /></span></div>
+                                            </div>
+                                            <div class="contact-email">
+                                                <div><span class="wpcf7-form-control-wrap your-email"><input
+                                                            type="email" name="your-email" value="" size="40"
+                                                            class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email"
+                                                            aria-required="true" aria-invalid="false"
+                                                            placeholder="Your email *" /></span></div>
+                                            </div>
+                                            <div class="contact-subject">
+                                                <div><span class="wpcf7-form-control-wrap your-subject"><input
+                                                            type="text" name="your-subject" value="" size="40"
+                                                            class="wpcf7-form-control wpcf7-text" aria-invalid="false"
+                                                            placeholder="Subject" /></span></div>
+                                            </div>
+                                            <div class="contact-message">
+                                                <div><span class="wpcf7-form-control-wrap your-message"><textarea
+                                                            name="your-message" cols="40" rows="10"
+                                                            class="wpcf7-form-control wpcf7-textarea"
+                                                            aria-invalid="false"
+                                                            placeholder="Message"></textarea></span></div>
+                                            </div>
+                                            <div class="contact-submit">
+                                                <input type="submit" value="Send Message" class="wpcf7-form-control has-spinner wpcf7-submit" />
+                                            </div>
+                                        </div>
+                                        <div class="wpcf7-response-output" aria-hidden="true"></div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="wpb_column vc_column_container vc_col-sm-2">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</main>
+</div>
+<!-- Close HARU Content Main -->
+
 
 
 <?php include("footer.php"); ?>
